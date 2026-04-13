1d5e78ec7f55ea71215e9e6f9a3b0cfd233e3590
vishw596 <vishwpatel76@outlook.com>
2026-04-13 16:44:54 +0530

    format task detail page

--- src/components/TaskDetails.tsx
+++ src/components/TaskDetails.tsx
@@ -1,10 +1,16 @@
-import  { useParams } from "react-router-dom"
+import  { Navigate, useParams } from "react-router-dom"
 import { useTask } from "../context/TaskProvider"
 
 export default function TaskDetails(){
     const {tasks} = useTask()
-    const {id} = useParams()    
+    const {id} = useParams() 
+    const task = tasks.find(({id:tid}) => tid === id);
+    if(!task){
+        return <Navigate to={"/"} replace/> 
+    }   
     return <>
-        {id}
+        <h1>{task.title}</h1>
+        <p>{task.description}</p>
+        <p>Status:{}</p>
     </>
 }
